bf19970c147c20a19d223e34e0740e365419a4ee
albertmoon <albertmoon@CELESTIAL>
2024-04-07 22:00:17 +0300

    Add project files.

---
+++ SCHOOL PORTAL V1.0.0.2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SCHOOL_PORTAL_V1._0._0._2.Global" Language="C#" %>
---
+++ SCHOOL PORTAL V1.0.0.2/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace SCHOOL_PORTAL_V1._0._0._2
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
